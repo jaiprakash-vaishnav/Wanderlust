622a112c3ce8c19d0791f0e78ae81028553b7b90
jaiprakash-vaishnav <jaiprakashv920@gmail.com>
2025-10-27 09:27:37 +0530

    Joi changes

--- schema.js
+++ schema.js
@@ -7,7 +7,7 @@ module.exports.listingSchema = Joi.object({
         location : Joi.string().required(),
         country : Joi.string().required(),
         price : Joi.number().required().min(0),
-        image : Joi.string().required(),
+        image : Joi.string().allow("",null),
         category : Joi.string().required().valid("trending","rooms","iconic-cities","mountains","castles","amazing-pools","camping","farms","arctic","domes","boats"),
     }).required()
 });
